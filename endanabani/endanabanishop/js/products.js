d831065ef2a9f6a018d2db68518b011b11119ee4
Cristian-Hernandez-PX <98490929+Cristian-Hernandez-PX@users.noreply.github.com>
2023-01-07 00:08:06 -0600

    Se modificaron las vistas con la informacion correspoiendiente que fue entragada por el cliente y se empezo el listado de los productos

---
+++ endanabani/endanabanishop/js/products.js
@@ -0,0 +1,257 @@
+[{
+        "Menudeo":[
+            {
+                "name": "Tlayudas",
+                "description": "Tortilla de maíz precosida para elaborar el tradicional platillo oaxaqueño del mismo nombre",
+                "presentation": "Pza de 35-40 cm de diámetro",
+                "price": 10.00,
+                "category": "Derivados de Maíz",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tlayudas",
+                "description": "Tortilla de maíz precosida para elaborar el tradicional platillo oaxaqueño del mismo nombre",
+                "presentation": "Pza 20-25 cm de diametro",
+                "price": 5.50,
+                "category": "Derivados de Maíz",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Totopos istmeños",
+                "description": "Tostadas de maíz de 12 cm de diámetro para acompañarse con frijoles refritos y salsas",
+                "presentation": "Paquete de 50 piezas",
+                "price": 110.00,
+                "category": "Derivados de Maíz",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tejate",
+                "description": "Bebida prehispánica de origen oaxaqueño, principalmente de las culturas mixteca y zapoteca. Está elaborada principalmente de maíz, granos y flor de cacao, y semillas de mamey.",
+                "presentation": "Polvo en: 250g.",
+                "price": 50.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tejate",
+                "description": "Bebida prehispánica de origen oaxaqueño, principalmente de las culturas mixteca y zapoteca. Está elaborada principalmente de maíz, granos y flor de cacao, y semillas de mamey.",
+                "presentation": "Polvo en: 500g.",
+                "price": 97.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tejate",
+                "description": "Bebida prehispánica de origen oaxaqueño, principalmente de las culturas mixteca y zapoteca. Está elaborada principalmente de maíz, granos y flor de cacao, y semillas de mamey.",
+                "presentation": "Polvo en: 1kg.",
+                "price": 180.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tascalate",
+                "description": "Bebida prehispánica típica del sur de Oaxaca y del Estado de Chiapas. Está elaborada de maíz tostado, achiote y cacao.",
+                "presentation": "Polvo en: 250g.",
+                "price": 50.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tascalate",
+                "description": "Bebida prehispánica típica del sur de Oaxaca y del Estado de Chiapas. Está elaborada de maíz tostado, achiote y cacao.",
+                "presentation": "Polvo en: 500g.",
+                "price": 97.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tascalate",
+                "description": "Bebida prehispánica típica del sur de Oaxaca y del Estado de Chiapas. Está elaborada de maíz tostado, achiote y cacao.",
+                "presentation": "Polvo en: 1kg.",
+                "price": 190.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Pozol",
+                "description": "Bebida prehispánica de la cultura maya. Actualmente es tradicional en los estados de Veracruz, Tabasco y Chiapas. Está elaborada a partir de maíz tostado y cacao.",
+                "presentation": "Polvo en: 250g.",
+                "price": 65.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Pozol",
+                "description": "Bebida prehispánica de la cultura maya. Actualmente es tradicional en los estados de Veracruz, Tabasco y Chiapas. Está elaborada a partir de maíz tostado y cacao.",
+                "presentation": "Polvo en: 500g.",
+                "price": 122.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Pozol",
+                "description": "Bebida prehispánica de la cultura maya. Actualmente es tradicional en los estados de Veracruz, Tabasco y Chiapas. Está elaborada a partir de maíz tostado y cacao.",
+                "presentation": "Polvo en: 1kg.",
+                "price": 230.00,
+                "category": "Bebidas prehispánicas",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Cecina de res",
+                "description": "Carne salada y suave que se obtiene del limo y la pierna de res.",
+                "presentation": "500g",
+                "price": 130.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Cecina de res",
+                "description": "Carne salada y suave que se obtiene del limo y la pierna de res.",
+                "presentation": "1kg",
+                "price": 250.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tasajo",
+                "description": "Corte de carne de vacuno tradicional de los Valles Centrales en Oaxaca, caraterizado por ser ahumado. Se acompaña de tlayudas, rábanos, chiles de agua, chapulines y quesillo.",
+                "presentation": "500g",
+                "price": 130.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Tasajo",
+                "description": "Corte de carne de vacuno tradicional de los Valles Centrales en Oaxaca, caraterizado por ser ahumado. Se acompaña de tlayudas, rábanos, chiles de agua, chapulines y quesillo.",
+                "presentation": "1kg",
+                "price": 230.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Cecina natural",
+                "description": "Carne obtenida de lomo de cerdo parecida a la cecina de res.",
+                "presentation": "500g",
+                "price": 90.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Cecina natural",
+                "description": "Carne obtenida de lomo de cerdo parecida a la cecina de res.",
+                "presentation": "1kg",
+                "price": 175.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Cecina enchilada",
+                "description": "Carne de lomo de cerdo preprarada con chile guajillo.",
+                "presentation": "500g",
+                "price": 90.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Cecina enchilada",
+                "description": "Carne de lomo de cerdo preprarada con chile guajillo.",
+                "presentation": "1kg",
+                "price": 175.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Chorizo Oaxaqueño",
+                "description": "Carne de cerdo molida, vinagre, chile pasilla, canela, hierbas de olor y pimienta. Platillo tradicional oaxaqueño para acompañar a las tlayudas.",
+                "presentation": "500g",
+                "price": 90.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Chorizo Oaxaqueño",
+                "description": "Carne de cerdo molida, vinagre, chile pasilla, canela, hierbas de olor y pimienta. Platillo tradicional oaxaqueño para acompañar a las tlayudas.",
+                "presentation": "1kg",
+                "price": 165.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Asiento de cerdo",
+                "description": "Sedimento hecho de la manteca quemada de cerdo con frituras provenientes del chicarrón. Se utiliza para las tradicionales memelitas o tlayudas.",
+                "presentation": "250g",
+                "price": 35.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Quesillo",
+                "description": "Producto lácteo elaborado a partir de la cuajada y de los sólidos de la leche fresca, que se pasan por un proceso de cocción, asegurando su inocuidad; se le agreda un poco de sal y se enrolla. Se utiliza para la elaboración de tlayudas y otros platillos típicos mexicanos.",
+                "presentation": "500g",
+                "price": 85.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Quesillo",
+                "description": "Producto lácteo elaborado a partir de la cuajada y de los sólidos de la leche fresca, que se pasan por un proceso de cocción, asegurando su inocuidad; se le agreda un poco de sal y se enrolla. Se utiliza para la elaboración de tlayudas y otros platillos típicos mexicanos.",
+                "presentation": "1kg",
+                "price": 150.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Queso de Chiapas",
+                "description": "Queso doble  crema tradicional del estado de Chiapas. Característico por su sabor fuerte, es delicioso para la preparación de quesadillas, acompañar enchiladas, tostadas y otros platillos mexicanos.",
+                "presentation": "1kg",
+                "price": 270.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Queso fresco",
+                "description": "Queso de rancho fresco en forma redonda, con textura cremosa. Excelente opción para acompañar antojitos y platillos tradionales mexicanos.",
+                "presentation": "500g",
+                "price": 65.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            },
+            {
+                "name": "Queso fresco",
+                "description": "Queso de rancho fresco en forma redonda, con textura cremosa. Excelente opción para acompañar antojitos y platillos tradionales mexicanos.",
+                "presentation": "1kg",
+                "price": 120.00,
+                "category": "Carnes y lácteos.",
+                "status": 1,
+                "stock": 10
+            }
+        ],
+        "Mayoreo":[]
+}]
